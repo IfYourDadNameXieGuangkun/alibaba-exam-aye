661f270f48acccff09c835990d8461ed86adcf92
bw <baiwei@lppz.com>
2020-03-31 12:04:19 +0800

    Initial commit

---
+++ alibaba-exam-consumer/src/main/java/com/alibaba/consumer/controller/NacosConsumerController.java
@@ -0,0 +1,25 @@
+package com.alibaba.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class NacosConsumerController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping(value = "/echo/app/name")
+    public String echo(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-provider");
+        String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),"Success");
+        return restTemplate.getForObject(url,String.class);
+
+    }
+}
