430af0e7b2f5d66e0d5815fa6c714e2b1f884137
bw <baiwei@lppz.com>
2020-04-24 18:23:34 +0800

    nacos-config分环境

--- alibaba-exam-provider/src/main/java/com/alibaba/provider/controller/NacosProviderController.java
+++ alibaba-exam-provider/src/main/java/com/alibaba/provider/controller/NacosProviderController.java
@@ -1,18 +1,23 @@
 package com.alibaba.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NacosProviderController extends BaseController {
+    @Value("${rediso}")
+    private String redis;
+    @Value("${mysql0}")
+    private String mysql;
     @GetMapping(value = "echo/{message}")
     public String echo(@PathVariable String message){
         logger.info("info Provider time is in:{}",System.currentTimeMillis());
         logger.debug("debug Provider time is in:{}",System.currentTimeMillis());
         logger.error("error Provider time is in:{}",System.currentTimeMillis());
         logger.warn("warn Provider time is in:{}",System.currentTimeMillis());
-        return "hello Nacos Discovery "+message;
+        return "hello Nacos Discovery "+message+"=="+redis+"==="+mysql;
     }
 
 }
