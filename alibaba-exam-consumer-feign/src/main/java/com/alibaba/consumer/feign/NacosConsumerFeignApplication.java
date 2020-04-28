b9906f951cb3b420cf8b16cb121942efc8bdb7cc
bw <baiwei@lppz.com>
2020-04-28 23:30:10 +0800

    mybatis-plus 自动代码生成

--- alibaba-exam-consumer-feign/src/main/java/com/alibaba/consumer/feign/NacosConsumerFeignApplication.java
+++ alibaba-exam-consumer-feign/src/main/java/com/alibaba/consumer/feign/NacosConsumerFeignApplication.java
@@ -1,5 +1,6 @@
 package com.alibaba.consumer.feign;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan("com.alibaba.api.common.mapper")
 public class NacosConsumerFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumerFeignApplication.class,args);
