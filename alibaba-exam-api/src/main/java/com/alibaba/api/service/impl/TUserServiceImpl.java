5855deff264a3eee07e9f8a1803f6cb0532e9628
bw <baiwei@lppz.com>
2020-04-30 16:52:18 +0800

    继续集成 gateway 完成网关配置:1.网关调用provider(localhost:9000/nacos-provider/echo/admin?token=123); 2.网关调用consumer-feign-->provider(localhost:9000/nacos-consumer-feign/consumer/feign/provider/admin?token=123)

--- alibaba-exam-api/src/main/java/com/alibaba/api/service/impl/TUserServiceImpl.java
+++ alibaba-exam-api/src/main/java/com/alibaba/api/service/impl/TUserServiceImpl.java
@@ -4,12 +4,9 @@ package com.alibaba.api.service.impl;
 import com.alibaba.api.business.provider.TUser;
 import com.alibaba.api.mapper.TUserMapper;
 import com.alibaba.api.service.ITUserService;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 import java.util.Map;
 
 /**
